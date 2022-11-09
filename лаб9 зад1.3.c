e06106b3f07b15626fbf3e3a23b7d89e70f30a2f
Алина <112811472+Alnpchn@users.noreply.github.com>
2022-11-09 22:58:35 +0300

    Update лаб9 зад1.3.c

--- лаб9 зад1.3.c
+++ лаб9 зад1.3.c
@@ -1,15 +1,29 @@
 #include<stdio.h>
 #include<locale.h>
-#define ROW 4
-#define COL 6
+#define ROW 9
+#define COL 10
+#define ROW2 4
+#define COL2 6
 void main()
 {
+	setlocale(LC_ALL, "RUS");
+	puts("Задание 1.2");
 	int row, col;
-	int temp = 5;
 	printf("\n");
 	for (row = 1; row <= ROW; ++row)
 	{
 		for (col = 1; col <= COL; col++)
+			if (row >= col)
+				printf("%5d", col * row);
+		printf("\n");
+	}
+	puts("Задание 1.3");
+	int row2, col2;
+	int temp = 5;
+	printf("\n");
+	for (row2 = 1; row2 <= ROW2; row2++)
+	{
+		for (col2 = 1; col2 <= COL2; col2++)
 			printf("%5d", temp);
 		printf("\n");
 	}
